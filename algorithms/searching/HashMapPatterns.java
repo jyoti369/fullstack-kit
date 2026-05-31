2aaa0998b2dafcb02e36c3be9abd65fb79bcf45e
Debojyoti Mandal <debojyotimandal369@gmail.com>
2026-06-01 02:45:01 +0530

    add(algorithms): add hashmap and hashset patterns

--- algorithms/searching/HashMapPatterns.java
+++ algorithms/searching/HashMapPatterns.java
@@ -79,3 +79,86 @@ public class HashMapPatterns {
         System.out.println(longestConsecutive(new int[]{100,4,200,1,3,2})); // 4
     }
 }
+
+
+package algorithms.searching;
+
+import java.util.*;
+
+/**
+ * HashMap / HashSet Patterns
+ */
+public class HashMapPatterns {
+
+    // 1. Two Sum (unsorted)
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) return new int[]{map.get(complement), i};
+            map.put(nums[i], i);
+        }
+        return new int[]{-1, -1};
+    }
+
+    // 2. Group Anagrams
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (String s : strs) {
+            char[] chars = s.toCharArray(); Arrays.sort(chars);
+            String key = new String(chars);
+            map.computeIfAbsent(key, k -> new ArrayList<>()).add(s);
+        }
+        return new ArrayList<>(map.values());
+    }
+
+    // 3. Top K frequent elements
+    public static int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> freq = new HashMap<>();
+        for (int n : nums) freq.merge(n, 1, Integer::sum);
+        PriorityQueue<Integer> heap = new PriorityQueue<>(Comparator.comparingInt(freq::get));
+        for (int key : freq.keySet()) {
+            heap.offer(key);
+            if (heap.size() > k) heap.poll();
+        }
+        int[] result = new int[k];
+        for (int i = k-1; i >= 0; i--) result[i] = heap.poll();
+        return result;
+    }
+
+    // 4. Subarray sum equals K
+    public static int subarraySum(int[] nums, int k) {
+        Map<Integer, Integer> prefixCount = new HashMap<>();
+        prefixCount.put(0, 1);
+        int count = 0, sum = 0;
+        for (int num : nums) {
+            sum += num;
+            count += prefixCount.getOrDefault(sum - k, 0);
+            prefixCount.merge(sum, 1, Integer::sum);
+        }
+        return count;
+    }
+
+    // 5. Longest consecutive sequence
+    public static int longestConsecutive(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int n : nums) set.add(n);
+        int maxLen = 0;
+        for (int n : set) {
+            if (!set.contains(n - 1)) { // start of sequence
+                int curr = n, len = 1;
+                while (set.contains(curr + 1)) { curr++; len++; }
+                maxLen = Math.max(maxLen, len);
+            }
+        }
+        return maxLen;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(twoSum(new int[]{2,7,11,15}, 9))); // [0,1]
+        System.out.println(groupAnagrams(new String[]{"eat","tea","tan","ate","nat","bat"}));
+        System.out.println(Arrays.toString(topKFrequent(new int[]{1,1,1,2,2,3}, 2))); // [1,2]
+        System.out.println(subarraySum(new int[]{1,1,1}, 2));  // 2
+        System.out.println(longestConsecutive(new int[]{100,4,200,1,3,2})); // 4
+    }
+}
